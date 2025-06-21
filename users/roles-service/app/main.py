43d71f329681359f52863903423f7b611f14fd45
Maxtrax696 <yoshua.calahorrano1999@gmail.com>
2025-06-21 00:22:11 -0500

    reapir main.py

--- users/roles-service/app/main.py
+++ users/roles-service/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.routes import router
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "roles-service is running"}
+
 
 app.include_router(router)
