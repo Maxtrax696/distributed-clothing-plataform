b59927386c4527050d925eb6114d3b8946e0217c
Maxtrax696 <yoshua.calahorrano1999@gmail.com>
2025-06-29 19:20:48 -0500

    Repair registration-service by jwt

--- users/registration-service/app/services/registration_service.py
+++ users/registration-service/app/services/registration_service.py
@@ -9,7 +9,11 @@ class RegistrationService:
         if self.repo.email_exists(user_data.email):
             raise ValueError("Email already registered")
 
-        hashed_pw = bcrypt.hashpw(user_data.password.encode('utf-8'), bcrypt.gensalt())
+        hashed_pw = bcrypt.hashpw(
+            user_data.password.encode('utf-8'),
+            bcrypt.gensalt()
+        ).decode('utf-8')  # solo se decodifica el resultado final
+
         user_dict = user_data.dict()
         user_dict["password"] = hashed_pw
 
